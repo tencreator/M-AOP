cf020cc988ec4459a21907b23ae98dbd9daa2271
Max W <87267714+FuriousF0xGG@users.noreply.github.com>
2023-03-07 18:32:55 +0000

    Update config.lua

--- M-AOP/config.lua
+++ M-AOP/config.lua
@@ -1,15 +1,15 @@
 Config = {}
 
 Config.ScriptEnabled = true --Don't change this please, if you don't want my script in your server delete it but if you want to be awkward and just disable it set this to false
+Config.PTEnabled = true --Set to false if you don't want the peacetime script
 Config.Debug = true --Set to false if you don't want things logged in console, informs of what is going on etc
 
 Config.Hud = {
     x = .165,
     y = .95,
     scale = .45,
+    TextColour = "~b~",
 }
-
-Config.MinutesToTeleport = 5 -- Gives certain amount of minutes to head to AOP or leave banned area or will be teleported to assigned coords in Config.TeleportCoords.
 
 Config.TeleportCoords = { --The coords it will teleport the user to if they are not in AOP {for example if AOP is Los Santos DO NOT Teleport the user to coords in BLaine County, it WILL bug them...}
     {Sandy_Banned = {x = 2043.48, y = 3445.57, z = 43.78}},  --The coords it will teleport them to if Sandy Shores is banned
@@ -19,7 +19,8 @@ Config.TeleportCoords = { --The coords it will teleport the user to if they are 
     {Los_Santos = {x = -1031.7, y = -2730.71, z = 13.76}},    --The coords it will teleport them to if AOP is Los Santos
 }
 
-Config.AOP = {
-    Command = "aop",
+Config.Misc = {
+    AOPCommand = "aop",
+    PTCommand = "peacetime",
     DefaultAOP = "State Wide",
     }
