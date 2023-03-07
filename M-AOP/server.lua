5b004750566bd673ff21dc1fcd31b549e7fbbefb
Max W <87267714+FuriousF0xGG@users.noreply.github.com>
2023-03-07 18:32:41 +0000

    Update server.lua

--- M-AOP/server.lua
+++ M-AOP/server.lua
@@ -1,11 +1,15 @@
 if Config.ScriptEnabled then
 
-local AOP = Config.AOP.DefaultAOP
-RegisterCommand(Config.AOP.Command, function(source, args, raw)
+AOP = Config.Misc.DefaultAOP
+if Config.PTEnabled then 
+Peacetime = false 
+end
+
+RegisterCommand(Config.Misc.AOPCommand, function(source, args, raw)
     if IsPlayerAceAllowed(source, "furiousfoxgg.aop") then 
     if args[1] then
         TriggerClientEvent("M:ClearAOPBans", -1)
-        Announce("error", "AOP Has changed to \"" ..table.concat(args, " ").. "\" Please move there or you risk being teleported..")
+        Announce("error", "AOP Announcement", "AOP Has changed to \"" ..table.concat(args, " ").. "\" Please move there or you risk being teleported..")
         local FinalAOP = CapitalizeFirstLetterOfWord(table.concat(args, " "))
         AOP = FinalAOP
     if string.find(string.lower(table.concat(args, " ")), "sandy banned") then
@@ -32,11 +36,25 @@ RegisterCommand(Config.AOP.Command, function(source, args, raw)
     end
 end)
 
-function Announce(style, msg)
+if Config.PTEnabled then 
+RegisterCommand(Config.Misc.PTCommand, function(source, args, raw)
+    if IsPlayerAceAllowed(source, 'furiousfoxgg.aop') then
+        Peacetime = not Peacetime
+        TriggerClientEvent("M:SetPeacetime", -1, Peacetime)
+        if Peacetime then 
+        Announce("error", "Peacetime Announcement", "Peacetime is now Enabled. You may be kicked if you shoot.")
+        else
+        Announce("error", "Peacetime Announcement", "Peacetime is now Disabled. You may shoot with roleplay.")
+            end
+        end
+    end)
+end
+
+function Announce(style, title, msg)
     TriggerClientEvent('t-notify:client:Custom', -1, {
         style  =  style,
         duration  =  10000,
-        title  =  'AOP Announcement',
+        title  =  title,
         message  =  msg,
         sound  =  false
     })
@@ -71,6 +89,11 @@ AddEventHandler("M:ServerUpdateAOP", function()
    if string.find(AOP, "Los Santos") then
     TriggerClientEvent("M:CreatePolyAOP", -1, "LS")
    end
+end)
+
+RegisterNetEvent("M:ServerSetPeacetime")
+AddEventHandler("M:ServerSetPeacetime", function()
+    TriggerClientEvent("M:SetPeacetime", source, Peacetime)
 end)
 
 end
